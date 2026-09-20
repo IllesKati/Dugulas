8b3afbb1e9eb420114540e20a20f39b415cc9da2
IllesKati <illeskatalin@hotmail.com>
2026-09-20 09:42:08 +0200

    Új logó, háttér, szöveg módosítás.

--- script.js
+++ script.js
@@ -4,17 +4,11 @@ document.addEventListener("DOMContentLoaded", function () {
     const hamburgerMenu = document.querySelector(".hamburger-menu");
     const navLinks = document.querySelector(".nav-links");
 
-    hamburgerMenu.addEventListener("click", function () {
-        navLinks.classList.toggle("active");
-    });
-});
-
-
-/*Accordion*/
-
-document.getElementById('headingOne').addEventListener('click', function () {
-    var expanded = this.getAttribute('aria-expanded') === 'true' || false;
-    this.setAttribute('aria-expanded', !expanded);
+    if (hamburgerMenu && navLinks) {
+        hamburgerMenu.addEventListener("click", function () {
+            navLinks.classList.toggle("active");
+        });
+    }
 });
 
 
